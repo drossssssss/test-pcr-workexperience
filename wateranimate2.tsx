fc542f20374836cbb4ad4b5109f2f7138b442602
LEPINE Manon <manon.lepine@oresys.eu>
2021-04-26 23:17:13 +0200

    Waterfall added

---
+++ wateranimate2.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="wateranimate2" tilewidth="32" tileheight="32" tilecount="10" columns="2">
+ <image source="wateranimate2.png" width="86" height="166"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="170"/>
+   <frame tileid="2" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="170"/>
+   <frame tileid="3" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="170"/>
+   <frame tileid="4" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="170"/>
+   <frame tileid="5" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="170"/>
+   <frame tileid="6" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="170"/>
+   <frame tileid="7" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="170"/>
+   <frame tileid="0" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="170"/>
+   <frame tileid="1" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="6" duration="170"/>
+   <frame tileid="8" duration="170"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="7" duration="170"/>
+   <frame tileid="9" duration="170"/>
+  </animation>
+ </tile>
+</tileset>
