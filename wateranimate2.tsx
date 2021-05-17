0249fa87dbd091f5ce9893ab87329fa4973db42e
drossssssss <82377830+drossssssss@users.noreply.github.com>
2021-05-17 15:14:55 +0200

    Update wateranimate2.tsx

--- wateranimate2.tsx
+++ wateranimate2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="wateranimate2" tilewidth="32" tileheight="32" tilecount="10" columns="2">
- <image source="wateranimate2.png" width="86" height="166"/>
+ <image source="..\/test-pcr-workexperience\/wateranimate2.png" width="86" height="166"/>
  <tile id="0">
   <animation>
    <frame tileid="0" duration="170"/>
